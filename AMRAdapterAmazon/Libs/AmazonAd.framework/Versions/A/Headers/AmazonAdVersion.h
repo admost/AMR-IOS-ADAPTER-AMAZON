482ad9021ef9b714dd6995f085a65e465e45f0b3
Mehmet Karagöz <eng.mkaragoz@gmail.com>
2020-07-16 21:28:59 +0300

    updated

--- AMRAdapterAmazon/Libs/AmazonAd.framework/Versions/A/Headers/AmazonAdVersion.h
+++ AMRAdapterAmazon/Libs/AmazonAd.framework/Versions/A/Headers/AmazonAdVersion.h
@@ -5,5 +5,5 @@
 //  Copyright (c) 2016 Amazon.com. All rights reserved.
 //
 #ifndef AMAZON_AD_SDK_VERSION_NUMBER
-#define AMAZON_AD_SDK_VERSION_NUMBER  "3.0.0"
+#define AMAZON_AD_SDK_VERSION_NUMBER  "3.1.0"
 #endif
